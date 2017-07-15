27320bbd60763b5201864cde8ca2957c14dc386b
Ravasz <graygreengold@gmail.com>
2017-07-15 13:06:12 +0100

    example run updated

--- documents/example_run_container.py/container.py
+++ documents/example_run_container.py/container.py
@@ -5,24 +5,24 @@ from instrumentino.action import SysAction, SysActionParamTime, SysActionParamFl
 from instrumentino.controllers.labsmith_eib.labsmith_comps import LabSmithValves4VM01
 from instrumentino.controllers.arduino.parker import ParkerPressureController
 from instrumentino.controllers.labsmith_eib import SysVarDigitalLabSmith_AV201Position
- 
+
 '''
 *** System constants
 '''
 # Arduino pin assignments
 pinAnalInParkerP = 5
 pinPwmOutParkerP = 6
- 
+
 # 3-way LabSmith Valves can be set to 'A', 'B' and 'closed'
 valvePortPressure = 'A'
 valvePortExhaust = 'B'
- 
+
 '''
 *** System components
 '''
 pressureController = ParkerPressureController('Pressure', (0,100), pinAnalInParkerP, pinPwmOutParkerP)
 valves = LabSmithValves4VM01('valves', (SysVarDigitalLabSmith_AV201Position('V1', 4, 'Help text about valve 1'),))
- 
+
 '''
 *** System actions
 '''
@@ -31,38 +31,38 @@ class SysActionFillContainer(SysAction):
         self.seconds = SysActionParamTime()
         self.psi = SysActionParamFloat(pressureController.vars['P'])
         SysAction.__init__(self, 'Fill Container', (self.seconds, self.psi))
- 
+
     def Command(self):
         # Connect container to pressure controller
         valves.vars['V1'].Set(valvePortPressure)
         # Start pressure
         pressureController.vars['P'].Set(self.psi.Get())
-         
+
         # Wait some time
         cfg.Sleep(self.seconds.Get())
-        
+
         # Close container
         valves.vars['V1'].Set('closed')
- 
- 
+
+
 class SysActionEmptyContainer(SysAction):
     def __init__(self):
         self.seconds = SysActionParamTime()
         SysAction.__init__(self, 'Empty Container', ())
- 
+
     def Command(self):
         # Stop pressure
         pressureController.vars['P'].Set(0)
-        
+
         # Connect container to pressure controller
         valves.vars['V1'].Set(valvePortExhaust)
-         
+
         # Wait some time
         cfg.Sleep(2)
-         
+
         # Close container
         valves.vars['V1'].Set('closed')
- 
+
 '''
 *** System
 '''
@@ -74,12 +74,12 @@ class System(Instrument):
         name = 'Example System'
         description = 'A container connected, through a valve, to a pressure controller or to the atmosphere'
         version = '1.0'
-         
+
         Instrument.__init__(self, comps, actions, version, name, description)
- 
+
 '''
 *** Run program
-'''        
+'''
 if __name__ == '__main__':
     # run the program
     System()
